06ed2a55c6f71e7d9de3a59236e35bb7f83ee307
todvob <todvob@gmail.com>
2024-07-09 13:43:23 +0600

    update chatbot git push

--- app/dashboard/[chatbotId]/page.tsx
+++ app/dashboard/[chatbotId]/page.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { User } from "lucide-react";
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Chatbot = ({ params }: { params: { chatbotId: string } }) => {
   const [messages, setMessages] = useState<
     { role: "user" | "ai"; content: string }[]
-  >([]);
+  >([
+    {
+      role: "user",
+      content: "arfat",
+    },
+    { role: "ai", content: "rahman" },
+  ]);
   const [loading, setLoading] = useState(false);
   const handleSubmit = async (e: any) => {
     setLoading(true);
@@ -30,40 +38,54 @@ const Chatbot = ({ params }: { params: { chatbotId: string } }) => {
   };
   return (
     <>
-      <div className="w-full md:w-96">
-        <div className="">
-          <img src="/next.svg" className="logo" />
-          <p className="sub-heading">Knowledge Bank</p>
-        </div>
-        <div className="h-96 overflow-y-auto w-full">
-          {messages.map((message, i) => (
-            <div
-              className={`w-full text-wrap ${
-                message.role === "user" ? "bg-pink-600" : "bg-gray-500"
-              }`}
-              key={i}
+      <div className="w-full h-[100vh-30px] flex justify-center items-center">
+        <div className="w-full md:w-96 border p-4 ">
+          <div className="">
+            <img src="/next.svg" className="logo" />
+            <p className="sub-heading">Knowledge Bank</p>
+          </div>
+          <div className="h-96 overflow-y-auto w-full gap-4 flex flex-col">
+            {messages.map((message, i) => (
+              <div
+                className={`w-full text-wrap flex gap-2 items-center shadow-lg rounded-lg py-4
+              `}
+                key={i}
+              >
+                <div className="bg-first w-6 h-6 rounded-full">
+                  {message.role === "user" ? (
+                    <Image
+                      alt="Arfat AI"
+                      src={"/logo-only.png"}
+                      className="bg-first rounded-full"
+                      width={30}
+                      height={30}
+                    />
+                  ) : (
+                    <User />
+                  )}
+                </div>
+                {message.content}
+              </div>
+            ))}
+            {loading && <div className="w-full ">thinking....</div>}
+          </div>
+          <form id="form" onSubmit={handleSubmit} className="flex w-full">
+            <input
+              name="userInput"
+              type="text"
+              className="p-2 border border-gray-600 rounded-l-lg flex-1 dark:bg-white dark:text-black"
+              required
+            />
+            <button
+              id="submit-btn"
+              type="submit"
+              disabled={loading}
+              className="text-white px-11 bg-green-600 rounded-r-lg"
             >
-              {message.content}
-            </div>
-          ))}
-          {loading && <div className="w-full ">thinking....</div>}
+              Send
+            </button>
+          </form>
         </div>
-        <form id="form" onSubmit={handleSubmit} className="flex w-full">
-          <input
-            name="userInput"
-            type="text"
-            className="p-2 border border-gray-600 rounded-l-lg flex-1 dark:bg-white dark:text-black"
-            required
-          />
-          <button
-            id="submit-btn"
-            type="submit"
-            disabled={loading}
-            className="text-white px-11 bg-green-600 rounded-r-lg"
-          >
-            Send
-          </button>
-        </form>
       </div>
     </>
   );
